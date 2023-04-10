05cbec9f25d3b0b6302c93adb9c0136396c7a21c
looping13 <olivier.dendayne@gmail.com>
2023-04-10 23:06:14 +0200

    tiny change to xfce_monPacman.sh

--- xfce_monPacman.sh
+++ xfce_monPacman.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-listPackage=$(checkupdates | awk -F" " '{print $1}' | sort)
+listPackage=$(checkupdates | awk -F" " '{print $1,$3,$4}' | sort)
 nbPackage=$(printf "%s" "$listPackage" | grep -c "^")
 
 # the genmon
@@ -10,12 +10,12 @@ then
   echo "<tool>No package update available</tool>"
 elif [ $nbPackage -eq 1 ]
 then
-  echo "<txt><span fgcolor='Red'>󰏔</span> $nbPackage </txt>"
+  echo "<txt><span fgcolor='Red'>󰏔</span><span weight='Bold'> $nbPackage </span></txt>"
   echo "<tool>$nbPackage package update available"
   echo "$listPackage</tool>"
 elif [ $nbPackage -gt 1 ]
 then
-  echo "<txt><span fgcolor='Red'>󰏔</span> $nbPackage </txt>"
+  echo "<txt><span fgcolor='Red'>󰏔</span><span weight='Bold'> $nbPackage </span></txt>"
   echo "<tool>$nbPackage packages update available"
   echo "$listPackage</tool>"
 fi
