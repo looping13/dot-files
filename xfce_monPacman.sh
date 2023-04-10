14d55e5a018dc02d1ea2a33644e88251425f9883
looping13 <olivier.dendayne@gmail.com>
2023-04-10 13:58:57 +0200

    adding xfce Pacman genmon script

--- xfce_monPacman.sh
+++ xfce_monPacman.sh
@@ -1,6 +1,21 @@
 #!/usr/bin/bash
 
-listPackage=$(checkupdates -n | awk -F" " '{print $1}' | sort)
-nbPackage=$($listPackage | wc -l)
+listPackage=$(checkupdates | awk -F" " '{print $1}' | sort)
+nbPackage=$(printf "%s" "$listPackage" | grep -c "^")
 
-echo $nbPackage "package update available" ; echo ${listPackage}
+# the genmon
+if [ $nbPackage -eq 0 ]
+then
+  echo "<txt><span fgcolor='Green'>󰏓 </span></txt>"
+  echo "<tool>No package update available</tool>"
+elif [ $nbPackage -eq 1 ]
+then
+  echo "<txt><span fgcolor='Red'>󰏔</span> $nbPackage </txt>"
+  echo "<tool>$nbPackage package update available"
+  echo "$listPackage</tool>"
+elif [ $nbPackage -gt 1 ]
+then
+  echo "<txt>><span fgcolor='Red'>󰏔</span> $nbPackage </txt>"
+  echo "<tool>$nbPackage packages update available"
+  echo "$listPackage</tool>"
+fi
