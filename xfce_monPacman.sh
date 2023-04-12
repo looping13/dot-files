923f94fa188e32d8fa5528194a450fabe9462696
Olivier Dendayne <olivier.dendayne@gmail.com>
2023-04-12 18:43:33 +0200

    pacman icons

--- xfce_monPacman.sh
+++ xfce_monPacman.sh
@@ -6,16 +6,16 @@ nbPackage=$(printf "%s" "$listPackage" | grep -c "^")
 # the genmon
 if [ $nbPackage -eq 0 ]
 then
-  echo "<txt><span fgcolor='Green'>󰏓 </span></txt>"
+  echo "<txt><span fgcolor='Yellow'>󰮯</span><span fgcolor='Green'>󰊠</span></txt>"
   echo "<tool>No package update available</tool>"
 elif [ $nbPackage -eq 1 ]
 then
-  echo "<txt><span fgcolor='Red'>󰏔</span><span weight='Bold'> $nbPackage </span></txt>"
+  echo "<txt><span fgcolor='Yellow'>󰮯</span><span fgcolor='Red'>󰊠 </span><span weight='Bold'> $nbPackage </span></txt>"
   echo "<tool>$nbPackage package update available"
   echo "$listPackage</tool>"
 elif [ $nbPackage -gt 1 ]
 then
-  echo "<txt><span fgcolor='Red'>󰏔</span><span weight='Bold'> $nbPackage </span></txt>"
+  echo "<txt><span fgcolor='Yellow'>󰮯</span><span fgcolor='Red'>󰊠 </span><span weight='Bold'> $nbPackage </span></txt>"
   echo "<tool>$nbPackage packages update available"
   echo "$listPackage</tool>"
 fi
