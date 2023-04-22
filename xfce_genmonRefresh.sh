1fc4ad1584f3d26166da60408500faaaf90c7d7f
looping13 <olivier.dendayne@gmail.com>
2023-04-22 09:37:49 +0200

    xfce genmon final

---
+++ xfce_genmonRefresh.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/bash
+bash -c "/usr/bin/xfce4-panel -r"
+#bash -c "/usr/bin/xfce4-panel --plugin-event=genmon-27:refresh:bool:true"
--- xfce_monPacman.sh
+++ xfce_monPacman.sh
@@ -1,21 +1,26 @@
 #!/usr/bin/bash
 
-listPackage=$(checkupdates | awk -F" " '{print $1,$3,$4}' | sort)
+listPackage=$(/usr/bin/checkupdates | awk -F" " '{print $1,$3,$4}' | sort)
 nbPackage=$(printf "%s" "$listPackage" | grep -c "^")
-
+TIME=$(date +%T)
 # the genmon
 if [ $nbPackage -eq 0 ]
 then
   echo "<txt><span fgcolor='Yellow'>󰮯</span><span fgcolor='Green'>󰊠</span></txt>"
-  echo "<tool>No package update available</tool>"
+  echo "<tool>Last checked at $TIME"
+  echo "No package update available</tool>"
 elif [ $nbPackage -eq 1 ]
 then
   echo "<txt><span fgcolor='Yellow'>󰮯</span><span fgcolor='Red'>󰊠 </span><span weight='Bold'> $nbPackage </span></txt>"
-  echo "<tool>$nbPackage package update available"
+  echo "<tool>Last checked at $TIME"
+  echo "$nbPackage package update available"
   echo "$listPackage</tool>"
 elif [ $nbPackage -gt 1 ]
 then
   echo "<txt><span fgcolor='Yellow'>󰮯</span><span fgcolor='Red'>󰊠 </span><span weight='Bold'> $nbPackage </span></txt>"
-  echo "<tool>$nbPackage packages update available"
+  echo "<tool>Last checked at $TIME"
+  echo "$nbPackage packages update available"
   echo "$listPackage</tool>"
 fi
+ echo "<txtclick>/home/olivier/Projects/dot-files/xfce_genmonRefresh.sh</txtclick>"
+##echo "<txtclick>gedit</txtclick>"
