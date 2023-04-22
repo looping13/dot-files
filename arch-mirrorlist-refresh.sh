9376a95da5c2bf128f26a61dbac63bb4a9e80177
Olivier Dendayne <olivier.dendayne@gmail.com>
2023-04-22 12:46:02 +0200

    better arch-mirrorlist-refresh

--- arch-mirrorlist-refresh.sh
+++ arch-mirrorlist-refresh.sh
@@ -3,25 +3,26 @@
 # refresh the archlinux mirror list in France from the mirrorlist.pacnew file
 # run with root privileges
 tput setaf 2
-echo "Saving previous mirrorlist"
+echo "Saving a backup of previous mirrorlist. Using mirrolist.pacnew to update mirrors"
 cp /etc/pacman.d/mirrorlist.pacnew /etc/pacman.d/mirrorlist.backup
-cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.old
+BLAH=$(date -u +%Y-%m-%dT%H_%M_%S)
+cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist_$BLAH.old
 
 # uncomment the servers from France https
 echo "Filter France https servers"
-awk '/^## France$/{f=1; next}f==0{next}/^$/{exit}{print substr($0, 1);}' /etc/pacman.d/mirrorlist.backup > /etc/pacman.d/mirrorlist1.backup
-awk '/#Server = https:/' /etc/pacman.d/mirrorlist1.backup > /etc/pacman.d/mirrorlist2.backup
-sed -i 's/^#Server/Server/' /etc/pacman.d/mirrorlist2.backup
+awk '/^## France$/{f=1; next}f==0{next}/^$/{exit}{print substr($0, 1);}' /etc/pacman.d/mirrorlist.pacnew |
+awk '/#Server = https:/' |
+awk 'gsub(/^#Server/, "Server")' > /etc/pacman.d/mirrorlist.backup
 
 # sort the 10 fastest servers
 tput setaf 3
 echo "Ranking mirrors now. Be patient..."
-rankmirrors -n 10 /etc/pacman.d/mirrorlist2.backup > /etc/pacman.d/mirrorlist1
+rankmirrors -n 10 /etc/pacman.d/mirrorlist.backup > /etc/pacman.d/mirrorlist
 
 # final words
 rm -f /etc/pacman.d/mirrorlist*.backup
 tput setaf 2
-echo "Arch mirrors list from France was updated."
+echo "Arch https mirrors list from France was updated."
 tput setaf 3
 echo "Please run pacman -Syyu"
 tput sgr0
